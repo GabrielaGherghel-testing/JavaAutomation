7152781c47429fa8cb69404e547f958dc072c0e6
GabrielaGherghel <gabrielagherghel83@gmail.com>
2025-01-21 14:38:54 +0200

    commit Whille, Do While, For

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,62 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+  public static void main(String[] args) {
+    /* //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    for (int i = 1; i <= 5; i++) {
+        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        System.out.println("i = " + i);
+    }*/
+    // citireNote();
+    //printareValoriWhile();
+    //printareValoriDoWhile();
+      printaeValoriFor();
+  }
+
+  public static void citireNote() {
+    System.out.println("Introduceti nota.");
+    Scanner scanner = new Scanner(System.in);
+    int nota = scanner.nextInt();
+    while (nota <= 0 || nota > 10) {
+      System.out.println(
+          "Nota " + nota + " nu este intre 1 si 10. Te rog introdu o valoare valida.");
+      nota = scanner.nextInt();
     }
+    System.out.println("Nota " + nota + " este intre 1 si 10.");
+  }
+
+  public static void printareValoriWhile() {
+    int valoare = 6;
+    while (valoare <= 5) {
+      System.out.println("Valoarea este " + valoare);
+      valoare++;
+        System.out.println("Valoarea dupa parcurcerea de while este " + valoare);
+    }
+  }
+
+  public static void printareValoriDoWhile() {
+    int valoare = 6;
+    do {
+      System.out.println("Valoare este " + valoare);
+      valoare++;
+    } while (valoare <= 5);
+    System.out.println("Valoarea dupa parcurcerea de do-while este " + valoare);
+  }
+  public static void printaeValoriFor ()
+  {
+      //int valoare=1;
+      for(int valoare=1;valoare<=5;)
+      {
+      System.out.println("Valoarea este " + valoare);
+      valoare++;
+      }
+  }
 }
--- src/test/java/VariabilaMetoda/Masina.java
+++ src/test/java/VariabilaMetoda/Masina.java
@@ -3,41 +3,52 @@ package VariabilaMetoda;
 import org.testng.annotations.Test;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Masina {
-    public String marca;
+  public String marca;
 
-    public String model;
+  public String model;
 
-    public Integer anfabricatie;
+  public Integer anfabricatie;
 
-    @Test
-    public void metodaTest(){
-        dateMasina("Audi", "Q10", 2007);
-        dateMasina("BVM", "X10", 2008);
+  @Test
+  public void metodaTest() {
+    dateMasina("Audi", "Q10", 2007);
+    dateMasina("BVM", "X10", 2008);
 
-        calculMedia(7.0,8.0);
-        calculMedia(3.0,8.0);
+    calculMedia(7.0, 8.0);
+    calculMedia(3.0, 8.0);
+    citireNote();
+  }
+
+  public void dateMasina(String param1, String param2, Integer param3) {
+    marca = param1;
+    model = param2;
+    anfabricatie = param3;
+
+    System.out.println("Marca masinii este " + marca);
+    System.out.println("Modelul masinii este " + model);
+    System.out.println("Anul de fabricatie este " + anfabricatie);
+  }
+
+  // calculam media la 2 numere
+
+  public void calculMedia(Double nota1, Double nota2) {
+
+    Double rezultat = (nota1 + nota2) / 2;
+    System.out.println("Media celor 2 note este: " + rezultat);
+  }
+
+  public void citireNote() {
+
+    Scanner scanner = new Scanner(System.in);
+    int nota = scanner.nextInt();
+    while (nota < 0 || nota > 10) {
+      System.out.println(
+          "Nota " + nota + "trebuie intre 1 si 10. Te rog introdu o valoare valida.");
+      nota = scanner.nextInt();
     }
-
-    public void dateMasina(String param1, String param2, Integer param3) {
-        marca=param1;
-        model=param2;
-        anfabricatie=param3;
-
-        System.out.println("Marca masinii este " + marca);
-        System.out.println("Modelul masinii este " + model);
-        System.out.println("Anul de fabricatie este " + anfabricatie);
-
-    }
-
-//calculam media la 2 numere
-
-    public void calculMedia(Double nota1, Double nota2){
-
-    Double rezultat=(nota1+nota2)/2;
-    System.out.println("Media celor 2 note este: "+rezultat);
-
-    }
-
+    System.out.println("Nota" + nota + " este intre 1 si 10.");
+  }
 }
