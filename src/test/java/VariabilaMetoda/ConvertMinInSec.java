5f8438bce3b38fbc1f9e559311456103e301b410
GabrielaGherghel <gabrielagherghel83@gmail.com>
2025-01-21 10:25:09 +0200

    commit Convert Min to Sec

---
+++ src/test/java/VariabilaMetoda/ConvertMinInSec.java
@@ -0,0 +1,40 @@
+package VariabilaMetoda;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ConvertMinInSec {
+
+  // Variabila minute
+  public int minutes;
+
+  // Metoda setter pentru a seta valoarea minutelor
+  public void setMinutes(int minutes) {
+    this.minutes = minutes;
+  }
+
+  // Calculăm secunde și afișăm rezultatul
+  public void calcConvert() {
+    // Calculul secundei
+    int seconds = minutes * 60;
+
+    // Tipărim rezultatul
+    System.out.println(minutes + " minutes is equal to " + seconds + " seconds.");
+  }
+
+  @Test
+  public void testConvert() {
+    // Folosim setter pentru a seta valoarea de minute
+    setMinutes(8);
+
+    // Apelăm metoda pentru a face conversia și a afișa rezultatul
+    calcConvert();
+
+    // Verificăm rezultatul cu o aserțiune
+    int expectedSeconds = 8 * 60;
+    Assert.assertEquals(minutes * 60, expectedSeconds);
+
+    // Tipărim și valoarea calculată a secundei pentru confirmare
+    System.out.println("Calculated seconds for " + minutes + " minutes: " + expectedSeconds);
+  }
+}
